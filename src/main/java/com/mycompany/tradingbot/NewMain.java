274e1d7682a2ec3fdcdb4403bc55e12550564877
Cian O Toole <N00180562@iadt.ie>
2020-01-07 12:16:02 +0000

    made a thread to get the price

--- src/main/java/com/mycompany/tradingbot/NewMain.java
+++ src/main/java/com/mycompany/tradingbot/NewMain.java
@@ -6,9 +6,16 @@
 package com.mycompany.tradingbot;
 
 import java.io.IOException;
+import static java.lang.ProcessBuilder.Redirect.from;
+import static java.lang.ProcessBuilder.Redirect.to;
 import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
 
 /**
  *
@@ -20,12 +27,36 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
- Stock stock = YahooFinance.get("INTC");
-        BigDecimal price = stock.getQuote(true).getPrice();
-        System.out.println(price);
-        BigDecimal peg = stock.getStats().getPeg();
-        System.out.println(peg);
-        stock.getQuote().getVolume();
+
+//        Calendar from = Calendar.getInstance();
+//        Calendar to = Calendar.getInstance();
+
+//        Stock google = YahooFinance.get("GOOG");
+//        List<HistoricalQuote> googleHistQuotes = google.getHistory(from, to, Interval.WEEKLY);
+//        System.out.println(googleHistQuotes);
+
+        PriceAndTime i = new PriceAndTime();
+
+        Calendar calNewYork = Calendar.getInstance();
+        calNewYork.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+        System.out.println();
+        System.out.println("Time in New York: " + calNewYork.get(Calendar.HOUR_OF_DAY) + ":"
+                + calNewYork.get(Calendar.MINUTE) + ":" + calNewYork.get(Calendar.SECOND));
+        System.out.println();
+        
+        Calendar openTime = Calendar.getInstance();
+        Calendar closeTime = Calendar.getInstance();
+        closeTime.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+        
+        openTime.add(Calendar.HOUR_OF_DAY, 9);  
+        closeTime.set(Calendar.HOUR_OF_DAY, 16);
+        closeTime.get(Calendar.HOUR_OF_DAY);
+        
+        System.out.println(closeTime);
+//        Thread t1 = new Thread(i);
+//        t1.start();
+        
+        
     }
-    
+
 }
---
+++ src/main/java/com/mycompany/tradingbot/PriceAndTime.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tradingbot;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+/**
+ *
+ * @author N00180562
+ */
+public class PriceAndTime implements Runnable {
+
+    @Override
+    public void run() {
+        for (;;) {
+
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                return;
+            }
+            Stock stock;
+            try {
+                stock = YahooFinance.get("INTC");
+                BigDecimal price = stock.getQuote(true).getPrice();
+                System.out.println(price);
+                
+                Calendar closeTime = Calendar.getInstance();
+        closeTime.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+        
+      
+        
+                
+                Calendar calNewYork = Calendar.getInstance();
+                calNewYork.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+     
+                calNewYork.get(Calendar.HOUR_OF_DAY);
+                closeTime.set(Calendar.HOUR_OF_DAY, 16);
+                
+                if(calNewYork.get(Calendar.HOUR_OF_DAY) == closeTime.get(Calendar.HOUR_OF_DAY)){
+                    return;
+                }
+                
+            } catch (IOException ex) {
+                Logger.getLogger(PriceAndTime.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+}
